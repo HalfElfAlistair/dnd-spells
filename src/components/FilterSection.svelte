1f7666835f255bf3742b2c5d387f29fa00a8ede4
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-06-06 12:58:50 +0100

    hardcoding default filters section

---
+++ src/components/FilterSection.svelte
@@ -0,0 +1,20 @@
+<section>
+  <button>Show Filters</button>
+</section>
+
+<style>
+  section {
+    margin: 5%;
+    border: solid 2px #e67700;
+    padding: 5%;
+  }
+
+  button {
+    width: 100%;
+    padding: 10px;
+    border-radius: 5px;
+    border: none;
+    background-color: black;
+    color: white;
+  }
+</style>
