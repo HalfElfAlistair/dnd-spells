9eaae4c7fb86ab3357ccc8e7375a4fe64e914e03
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-06-06 13:18:53 +0100

    adding SpellsList component to App

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
 <script>
   import Header from "./components/Header.svelte";
   import FilterSection from "./components/FilterSection.svelte";
+  import SpellsList from "./components/SpellsList.svelte";
 </script>
 
 <Header />
 <FilterSection />
+<SpellsList />
 
 <style>
 </style>
