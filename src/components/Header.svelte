6cd07e10bab0a43f96b1041599a81d5f2af96f9b
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-06-06 12:31:47 +0100

    setting up svelte application and removing some default content

---
+++ src/components/Header.svelte
@@ -0,0 +1,18 @@
+<header>
+  <h1>DnD Spells (5E)</h1>
+</header>
+
+<style>
+  /* * {
+    border: solid 2px #364fc7;
+  } */
+
+  header {
+    margin: 5%;
+    border: solid 2px #364fc7;
+  }
+
+  h1 {
+    text-align: center;
+  }
+</style>
