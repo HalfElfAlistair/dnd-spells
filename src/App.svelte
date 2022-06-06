15355928f6b1cab8945e133bf6955d68854d269c
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-06-06 12:59:12 +0100

    adding FiltersSection to App

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,10 @@
 <script>
   import Header from "./components/Header.svelte";
+  import FilterSection from "./components/FilterSection.svelte";
 </script>
 
 <Header />
-<main />
+<FilterSection />
 
 <style>
 </style>
