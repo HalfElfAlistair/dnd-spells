c750506a759a73aea559dbcf340e8fc8529aacf0
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-06-06 13:18:22 +0100

    hardcoding SpellsList component, using SpellCard component

---
+++ src/components/SpellsList.svelte
@@ -0,0 +1,23 @@
+<script>
+  import SpellCard from "./SpellCard.svelte";
+</script>
+
+<main>
+  <ul>
+    <SpellCard />
+    <SpellCard />
+    <SpellCard />
+  </ul>
+</main>
+
+<style>
+  main {
+    margin: 5%;
+    border: solid 2px #c92a2a;
+  }
+
+  ul {
+    list-style: none;
+    padding-left: 0;
+  }
+</style>
