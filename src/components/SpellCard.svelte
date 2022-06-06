337f311710da818c58b90433e03829cebf341fc5
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-06-06 13:17:23 +0100

    hardcoding SpellCard component

---
+++ src/components/SpellCard.svelte
@@ -0,0 +1,29 @@
+<li>
+  <div class="spell-name-area">
+    <h2>Spell Name</h2>
+  </div>
+  <div class="level-class-area">
+    <p>Level</p>
+    <p>Class</p>
+  </div>
+</li>
+
+<style>
+  li {
+    margin: 5%;
+    border: solid 2px #0b7285;
+    padding-left: 5%;
+  }
+
+  h2,
+  p {
+    margin: 5px;
+  }
+
+  .level-class-area {
+    display: flex;
+    flex-direction: row;
+    justify-content: start;
+    gap: 30%;
+  }
+</style>
