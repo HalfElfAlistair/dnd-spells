2c4a9112e687554b57c160a497dbfc47a14dbdb8
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-06-13 18:09:05 +0100

    creating Nav component

---
+++ src/components/Nav.svelte
@@ -0,0 +1,31 @@
+<nav>
+  <a href="../App">All Spells</a>
+  <a>By Class</a>
+</nav>
+
+<style>
+  nav {
+    border: solid 2px #862e9c;
+    padding: 5%;
+    background-color: white;
+    width: 89%;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
+  a {
+    margin: 0 5%;
+    text-decoration: none;
+    font-size: 16px;
+    font-weight: bold;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+</style>
