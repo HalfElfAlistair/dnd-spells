ed5998e4d56d87c82a5e1d102034f4c4a767bca9
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-06-20 16:49:08 +0100

    adding ClassHeading component that will display a chosen class

---
+++ src/components/ClassHeading.svelte
@@ -0,0 +1,14 @@
+<script>
+  // export let selectedClass = "class 1";
+  import { selectedClass } from "../store-files/classSelection";
+</script>
+
+<h2>{$selectedClass} Spells</h2>
+
+<style>
+  h2 {
+    margin: 5%;
+    padding: 5%;
+    border: solid 2px hotpink;
+  }
+</style>
---
+++ src/components/ClassSelect.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { selectedClass } from "../store-files/classSelection";
+  function classSelector(event) {
+    console.log(event.target.value);
+    selectedClass.set(event.target.value);
+  }
+</script>
+
+<section>
+  <select on:change={classSelector}>
+    <option value="class 1">class 1</option>
+    <option value="class 2">class 2</option>
+  </select>
+</section>
+
+<style>
+  section {
+    margin: 5%;
+    border: solid 2px #e67700;
+    padding: 5%;
+  }
+
+  select {
+    width: 100%;
+    margin-top: 5%;
+    padding: 5px;
+    background-color: white;
+    color: black;
+    border: solid 2px black;
+    border-radius: 5px;
+  }
+</style>
