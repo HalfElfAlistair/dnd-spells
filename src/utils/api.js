e059fcea257616d66fca0a5610f463d0322e179b
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-07-01 15:05:03 +0100

    adding a new api request for the class path

--- src/utils/api.js
+++ src/utils/api.js
@@ -8,3 +8,14 @@ export const getSpells = async (query) => {
     const {data} = await dndSpellsApi.get(query ? `/spells?${query}` : "/spells")
     return data.results
 }
+
+export const getClasses = async () => {
+    const {data} = await dndSpellsApi.get("classes")
+    return data.results
+}
+
+export const getSpellsByClass = async (query) => {
+    console.log(query)
+    const {data} = await dndSpellsApi.get(`classes/${query}/spells`)
+    return data.results
+}
