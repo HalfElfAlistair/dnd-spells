e1391e95c55ae762250312de441df9be0b675b5d
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-07-01 15:07:02 +0100

    updating ClassSpells component to access api data for classes and feed this to relevant components which are now displayed

--- src/components/ClassSpells.svelte
+++ src/components/ClassSpells.svelte
@@ -1,1 +1,14 @@
-<h2>Class Spells</h2>
+<script>
+  import ClassHeading from "./ClassHeading.svelte";
+  import ClassSelect from "./ClassSelect.svelte";
+  import SpellsList from "./SpellsList.svelte";
+  import { getClasses } from "../utils/api";
+</script>
+
+<main>
+  {#await getClasses() then data}
+    <ClassSelect classList={data} />
+    <ClassHeading firstClassName={data[0].name} />
+    <SpellsList spellsComponent="class" />
+  {/await}
+</main>
